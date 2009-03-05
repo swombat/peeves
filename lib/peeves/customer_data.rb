5b8398ed30e6d692c311a8463b44d8223ca21f09
Zac Zheng <zac@zenpow.com>
2009-03-06 00:59:51 +0800

    payment now 2.23 compatibile
    Signed-off-by: Daniel Tenner <daniel.github@tenner.org>

---
+++ lib/peeves/customer_data.rb
@@ -0,0 +1,12 @@
+module Peeves
+  class CustomerData
+    # attr_accessor :address, :post_code, :name, :contact_number, :email
+    attr_accessor :surname, :firstnames, :address1, :address2, :city, :post_code, :country, :state, :phone, :email
+
+    def initialize(attrs = {})
+      attrs.each do |h,v|
+        send("#{h.to_s}=", v)
+      end
+    end
+  end
+end

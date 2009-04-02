f8deb9182cd57876bc477d932ead5b2949ca5176
Daniel Tenner <danieltenner@daniel-tenners-macbook-pro.local>
2009-04-02 12:57:15 +0100

    Temp prod investigation fix

--- lib/peeves/protx_services.rb
+++ lib/peeves/protx_services.rb
@@ -3,7 +3,7 @@ module Peeves
     BASE_URL = {
       :simulator  => 'https://ukvpstest.protx.com/VSPSimulator/VSPServerGateway.asp',
       :test       => 'https://ukvpstest.protx.com/vspgateway/service',
-      :live       => 'https://ukvps.protx.com/vspgateway/service'
+      :live       => 'https://ukvpstest.protx.com/showpost/'
     }
 
     SERVICE = {
@@ -32,7 +32,7 @@ module Peeves
         :cancel       => '/cancel.vsp'
       },
       :live => {
-        :payment      => '/vspserver-register.vsp',
+        :payment      => 'showpost.vsp',
         :release      => '/release.vsp',
         :abort        => '/abort.vsp',
         :refund       => '/refund.vsp',
